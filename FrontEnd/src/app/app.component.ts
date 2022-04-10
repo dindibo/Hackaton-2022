dacaa4589328393a10b5aef8ed58e0eda29c1891
Nikita Kokhan <kokhannikita01@gmail.com>
2022-04-10 14:57:37 +0300

    added heatmap

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import {} from 'googlemaps';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontEnd';
+  @ViewChild('map',{static:true}) mapElement: any;
+  map: google.maps.Map | null = null;
+
+  constructor(private http: HttpService) {}
+
+  ngOnInit(): void {
+
+    var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+   const mapProperties = {
+        center: sanFrancisco,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+   };
+   this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+   this.http.getPositions(new Date()).subscribe(arr => {
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: arr
+    });
+    heatmap.setMap(this.map);
+   })
+   
 }
+}
+
---
+++ FrontEnd/src/app/http.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {} from 'googlemaps';
+import { of,Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) {}
+
+  getPositions(date : Date):Observable<Array<google.maps.LatLng|google.maps.visualization.WeightedLocation>>{
+    return of([
+      {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+      new google.maps.LatLng(37.782, -122.445),
+      {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+      {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+      {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+      new google.maps.LatLng(37.782, -122.437),
+      {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+    
+      {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+      {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+      new google.maps.LatLng(37.785, -122.443),
+      {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+      new google.maps.LatLng(37.785, -122.439),
+      {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+      {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+    ]);
+  }
+}
